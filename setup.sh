81a28d8129b1fbde5f3d87dd831593cf8a1d0e0c
Floyd Zweydinger <floyd.zweydinger@rub.de>
2023-01-15 00:11:32 +0100

    fixxed setup script

--- setup.sh
+++ setup.sh
@@ -1,3 +1,5 @@
+mkdir deps
 cd deps
-wget https://www.intel.com/content/dam/develop/public/us/en/include/intrinsics-guide/data-latest.xml
+header='--header=User-Agent: Mozilla/5.0 (Windows NT 6.0) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.97 Safari/537.11'
+wget "$header" https://www.intel.com/content/dam/develop/public/us/en/include/intrinsics-guide/data-latest.xml
 wget https://www.uops.info/instructions.xml
